e3b180f13a16e520de04bc854d558bcd69072511
Rafał <rafalgulewski@gmail.com>
2021-11-01 01:46:52 +0100

    podział programu na funkcje

--- LAB3/skrypt.py
+++ LAB3/skrypt.py
@@ -2,16 +2,20 @@ from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 from Crypto.Protocol.KDF import PBKDF2
 
-def prepareinput(str, bl):
-    return str
+def prepareinput(_str, bl):
+    r = bl - len(_str) % bl
+    if(r != 0):
+        for i in range(r):
+            _str = _str + "0"
+    return _str
 
-def encryptWithAESCBC(str, passwd):
+def encryptWithAESCBC(_str, passwd):
     key = PBKDF2(passwd, b"salt")
     iv = get_random_bytes(16)
     print("wygenerowany klucz z hasla: ", key)
-    str = prepareinput(str, 16)
+    _str = prepareinput(_str, 16)
     aes = AES.new(key, AES.MODE_CBC, iv)
-    encrypted = aes.encrypt(str)
+    encrypted = aes.encrypt(str.encode(_str))
     return (encrypted, key, iv)
 
 def decryptWithAESCBC(str, key, iv):
@@ -20,11 +24,8 @@ def decryptWithAESCBC(str, key, iv):
     return decrypted
 
 
-
 wejscie = input("Podaj halso: ")
-
-#wczytanie pliku i przygo
-data = str.encode("Tekst do zaszyfrowania0000000000")
+data = "Tekst do zaszyfrowania"
 
 (e, k, i) = encryptWithAESCBC( data, wejscie)
 print("zaszyfrowany tekst: \n",  e)
