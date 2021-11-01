c3b9a3341e8e7543d2b600f5e7ae687231f92eb6
Rafał <rafalgulewski@gmail.com>
2021-11-01 09:50:26 +0100

    ...

--- LAB3/skrypt.py
+++ LAB3/skrypt.py
@@ -3,19 +3,22 @@ from Crypto.Random import get_random_bytes
 from Crypto.Protocol.KDF import PBKDF2
 
 def prepareinput(_str, bl):
+    _str = str.encode(_str, "utf-8")
     r = bl - len(_str) % bl
     if(r != 0):
+        _str = bytearray(_str)
         for i in range(r):
-            _str = _str + "0"
+            _str.append(r)
     return _str
 
 def encryptWithAESCBC(_str, passwd):
     key = PBKDF2(passwd, b"salt")
     iv = get_random_bytes(16)
     print("wygenerowany klucz z hasla: ", key)
+    print("wygenerowany wartość iniciująca: ", iv)
     _str = prepareinput(_str, 16)
     aes = AES.new(key, AES.MODE_CBC, iv)
-    encrypted = aes.encrypt(str.encode(_str))
+    encrypted = aes.encrypt(_str)
     return (encrypted, key, iv)
 
 def decryptWithAESCBC(str, key, iv):
@@ -25,9 +28,19 @@ def decryptWithAESCBC(str, key, iv):
 
 
 wejscie = input("Podaj halso: ")
-data = "Tekst do zaszyfrowania"
+in_file = input("Podaj nazwę pliku do zaszyfrowania: ")
 
+#data = "Tekst do zaszyfrowania"
+data = open(in_file, 'r').read()
+
+
+of = open('encrypted', 'wb')
 (e, k, i) = encryptWithAESCBC( data, wejscie)
+print("\n ---- ZASZYFROWANIE ----")
 print("zaszyfrowany tekst: \n",  e)
+of.write(e)
+of.close()
+
+print("\n ---- ODSZYFROWANIE ----")
 d = decryptWithAESCBC(e, k, i)
 print("odszyfrowany tekst: \n",d)
