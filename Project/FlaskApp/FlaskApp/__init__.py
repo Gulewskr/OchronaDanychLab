d85a963b9c6564652ca62076af57e9e26a3e32eb
rafalgulewski <rafalgulewski@gmail.com>
2022-01-17 01:55:22 +0100

    Przygotowanie serwera

--- Project/FlaskApp/FlaskApp/__init__.py
+++ Project/FlaskApp/FlaskApp/__init__.py
@@ -1,9 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello Flask under Apache!'
-
+@app.route("/")
+def hello():
+    return "Hello, I love Digital Ocean!"
 if __name__ == "__main__":
     app.run()
